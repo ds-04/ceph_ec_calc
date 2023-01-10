b97eb2ce149673cb1b2e86bcecd42f7f8d49ace6
David Simpson <31688862+ds-04@users.noreply.github.com>
2023-01-10 17:09:56 +0000

    Update ceph_ec_calc.py
    enable osd based calculation

--- ceph_ec_calc.py
+++ ceph_ec_calc.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 
-#Author David Simpson, 2022
-
 import argparse
 import sys
 
-parser = argparse.ArgumentParser(description="Ceph EC calculator",
+parser = argparse.ArgumentParser(description="Ceph EC calculator - calculate EC using servers,drives,capacity OR osds,capacity",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("-s", "--servers", type=int, default=6, help="number of servers")
-parser.add_argument("-d", "--drives", type=int, default=12, help="drives per server")
-parser.add_argument("-c", "--capacity", type=int, default=12, help="drive capacity (TB)")
+parser.add_argument("-d", "--drives", type=int, default=12, help="drives per server, where each is an OSD")
+parser.add_argument("-o", "--osds", type=int, default=0, help="total number of OSDs, this supersedes usage of servers*drives, capacity should still be used")
+parser.add_argument("-c", "--capacity", type=int, default=12, help="drive capacity (TB) - AKA OSD size")
 parser.add_argument("-k", "--chunks", type=int, default=4, help="EC chunks")
 parser.add_argument("-m", "--parity", type=int, default=2, help="EC parity chunks")
 parser.add_argument("-t", "--table", action='store_true', help="Print in tabulate form")
@@ -20,6 +19,7 @@ args = vars(parser.parse_args())
 
 #SERVERS/OSDs
 SERVER_COUNT=args["servers"]
+OSD_COUNT=args["osds"]
 
 #EC
 EC_K=args["chunks"]  #number of chunks to divide object into
@@ -32,6 +32,12 @@ DRIVE_PER_SERVER=args["drives"]
 
 #*** CHECKS *********************
 
+if OSD_COUNT < 0:
+   print("ERROR - CANT HAVE NEGATIVE OSD COUNT")
+   sys.exit(1)
+if OSD_COUNT == 1:
+   print("ERROR - CANT HAVE OSD COUNT OF 1")
+   sys.exit(1)
 if SERVER_COUNT < 1:
    print("ERROR - SERVER_COUNT SHOULD BE > 1")
    sys.exit(1)
@@ -52,32 +58,45 @@ if EC_M >= EC_K:
    print("ERROR - M PARITY MUST BE LESS THAN K CHUNKS")
    sys.exit(1)
 
-if EC_TOTAL > SERVER_COUNT:
-   print("ERROR - SERVER_COUNT SHOULD BE >= EC CHUNK TOTAL (K+M)")
-   print("SERVER_COUNT WAS "+str(SERVER_COUNT))
+if (EC_TOTAL > SERVER_COUNT and OSD_COUNT <= 0) or (EC_TOTAL > OSD_COUNT and OSD_COUNT > 1):
+   print("ERROR - SERVER_COUNT OR OSD_COUNT SHOULD BE >= EC CHUNK TOTAL (K+M)")
    print("K WAS "+str(EC_K))
    print("M WAS "+str(EC_M))
    print("EC CHUNK TOTAL WAS "+str(EC_TOTAL))
    sys.exit(1)
 
-# ***CALCULATIONS IN TB***
-SERVER_SIZE=(DRIVE_SIZE*DRIVE_PER_SERVER)
-RAW=(SERVER_COUNT*SERVER_SIZE)
 
+# ***CALCULATIONS **
 EFFICIENCY=float(EC_K/(float(EC_K+EC_M)))
 
-FULL_USAGE=float(SERVER_COUNT*EFFICIENCY*SERVER_SIZE)
+# ***CALCULATIONS CONT. (IN TB)***
+#use OSD count if specified
+if OSD_COUNT > 1:
+   RAW=(OSD_COUNT*DRIVE_SIZE)
+else:
+#if OSD_COUNT NOT USED, USE SERVER+DRIVES COUNTS
+   SERVER_SIZE=(DRIVE_SIZE*DRIVE_PER_SERVER)
+   RAW=(SERVER_COUNT*SERVER_SIZE)
+
+FULL_USAGE=float(EFFICIENCY*RAW)
 EIGHTY_FULL_USAGE=float(0.80*FULL_USAGE)
 
 if args["table"] != True:
-
    # ***OUTPUT ***
-   print("----")
-   print("-c "+str(DRIVE_SIZE)+" | Using DRIVE_SIZE (TB) "+str(DRIVE_SIZE))
-   print("-d "+str(DRIVE_PER_SERVER)+" | Using DRIVE_PER_SERVER "+str(DRIVE_PER_SERVER))
-   print("-s "+str(SERVER_COUNT)+" | Using SERVER_COUNT "+str(SERVER_COUNT))
-   print("----")
-   print("Results in SERVER_SIZE (TB) "+str(SERVER_SIZE))
+   if OSD_COUNT == 0:
+      print("----")
+      print("**** Using --servers NOT --osds ****")
+      print("----")
+      print("-c "+str(DRIVE_SIZE)+" | Using DRIVE|OSD_SIZE (TB) "+str(DRIVE_SIZE))
+      print("-d "+str(DRIVE_PER_SERVER)+" | Using DRIVE|OSD_PER_SERVER "+str(DRIVE_PER_SERVER))
+      print("-s "+str(SERVER_COUNT)+" | Using SERVER_COUNT "+str(SERVER_COUNT))
+      print("Results in SERVER_SIZE (TB) "+str(SERVER_SIZE))
+   if OSD_COUNT > 1:
+      print("----")
+      print("**** Using --osds NOT --servers ****")
+      print("----")
+      print("-c "+str(DRIVE_SIZE)+" | Using DRIVE|OSD_SIZE (TB) "+str(DRIVE_SIZE))
+      print("-o "+str(OSD_COUNT)+" | Using OSD_COUNT "+str(OSD_COUNT))
    print("Results in RAW (TB) "+str(RAW))
    print("----")
    print("-k "+str(EC_K)+" | Using K (min copy) of "+str(EC_K))
@@ -94,7 +113,10 @@ if args["table"] != True:
 
 if args["table"] == True:
    from tabulate import tabulate
-   table_output=[[str(DRIVE_SIZE),str(DRIVE_PER_SERVER),str(SERVER_COUNT),str(EC_K),str(EC_M),str(round(EFFICIENCY, 3)),str(round(FULL_USAGE, 3)),str(round(EIGHTY_FULL_USAGE, 3))]]
-   headers =['DriveSize(TB)','Drives','Servers','K','M','EFFICIENCY RATIO','100% CAPACITY (TB)','80% CAPACITY (TB)']
+   if OSD_COUNT == 0:
+      table_output=[[str(DRIVE_SIZE),str(DRIVE_PER_SERVER),str(SERVER_COUNT),str(EC_K),str(EC_M),str(round(EFFICIENCY, 3)),str(round(FULL_USAGE, 3)),str(round(EIGHTY_FULL_USAGE, 3))]]
+      headers =['Drive|OSD Size(TB)','Drives|OSD Per Server','Servers','K','M','EFFICIENCY RATIO','100% CAPACITY (TB)','80% CAPACITY (TB)']
+   if OSD_COUNT > 1:
+      table_output=[[str(DRIVE_SIZE),str(OSD_COUNT),str(EC_K),str(EC_M),str(round(EFFICIENCY, 3)),str(round(FULL_USAGE, 3)),str(round(EIGHTY_FULL_USAGE, 3))]]
+      headers =['Drive|OSD Size(TB)','nOSDs','K','M','EFFICIENCY RATIO','100% CAPACITY (TB)','80% CAPACITY (TB)']
    print(tabulate(table_output, headers, tablefmt="heavy_grid"))
-
